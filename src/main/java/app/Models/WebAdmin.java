54cd21736ec6c478565c932c7886f49f2ba4f3e4
Ahmed Wael <ahmedwaelwanas@gmail.com>
2023-09-05 18:10:08 +0300

    added missing relationship

--- src/main/java/app/Models/WebAdmin.java
+++ src/main/java/app/Models/WebAdmin.java
@@ -1,5 +1,15 @@
 package app.Models;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+import app.Models.Requests.Request;
+
 public class WebAdmin extends User{
+        @OneToMany(mappedBy = "admin", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Request> requests;
 
 }
